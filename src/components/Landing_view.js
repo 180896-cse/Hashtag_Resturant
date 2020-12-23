c43819c8660bfa4278a3e3eeb2c5c34220def99b
Shantanu_Pratap <62785261+180896-cse@users.noreply.github.com>
2020-12-23 23:34:10 +0530

    commited

---
+++ src/components/Landing_view.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../css/Landing_view.css";
+
+export default function Landing_view() {
+  return (
+    <div className="Landing_view">
+      <h1>hey</h1>
+      <h2></h2>
+    </div>
+  );
+}
