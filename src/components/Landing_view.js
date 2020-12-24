6c12462ed9c15bc0be89352f9f564faa6974dee4
Shantanu_Pratap <62785261+180896-cse@users.noreply.github.com>
2020-12-25 00:01:01 +0530

    commited on 25/12/2020

--- src/components/Landing_view.js
+++ src/components/Landing_view.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
+import ReactDom from "react-dom";
 import "../css/Landing_view.css";
+import main_bg from "../assets/26.jpg";
 
-export default function Landing_view() {
-  return (
-    <div className="Landing_view">
-      <h1>hey</h1>
-      <h2></h2>
-    </div>
-  );
+class Landing_view extends Component {
+  render() {
+    const imgStyle = {
+      backgroundImage: `url(${main_bg})`,
+      position: "fixed",
+      minWidth: "100%",
+      minHeight: "100%",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      filter: "blur(4px)"
+    };
+    return (
+      <section>
+        <div style={imgStyle}></div>
+        <div className="banner_tittle">
+          <h1>Restaurants and Drinks</h1>
+        </div>
+      </section>
+    );
+  }
 }
+
+export default Landing_view;
